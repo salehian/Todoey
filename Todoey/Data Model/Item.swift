323a1526b4bb5e0a32a2f773cfc74870c5f2c564
Mohammad Salehian <max@mohammads-macbook-pro.local>
2018-05-21 16:15:36 +0200

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mohammad on 5/8/18.
+//  Copyright © 2018 Mohammad Ali Salehian. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
